4d3b5b826001de15da6d06fb98d4bf62156ec827
Artur Sobczyk <artur.sobczyk83@outlook.com>
2020-05-10 00:12:48 +0200

    initial app version

---
+++ src/main/java/pl/arsob/aws/demo/ebsdemo/EbsDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.arsob.aws.demo.ebsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbsDemoApplication.class, args);
+	}
+
+}
